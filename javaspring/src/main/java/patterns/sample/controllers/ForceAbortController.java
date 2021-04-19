6f88fc65cac56c982e43395972412f9ec9e73530
Tuan Tran <4468525+huntertran@users.noreply.github.com>
2021-04-18 23:46:56 -0400

    finish for java springboot

---
+++ javaspring/src/main/java/patterns/sample/controllers/ForceAbortController.java
@@ -0,0 +1,46 @@
+package patterns.sample.controllers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import patterns.sample.services.TimeOutTask;
+
+@RestController
+public class ForceAbortController {
+
+    Future<String> future;
+
+    @GetMapping("/forceabort")
+    public String get() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        future = executor.submit(new TimeOutTask(10000));
+
+        try {
+            future.get();
+        } catch (Exception e) {
+            System.out.println("Task throw exception: " + e.toString());
+        }
+
+        return new String("ok");
+    }
+
+    @PostMapping("/forceabort")
+    public String post() {
+
+        if (future != null) {
+            boolean result = future.cancel(true);
+            if (result) {
+                return new String("task cancelled");
+            } else {
+                return new String("task is still running");
+            }
+        } else {
+            return "Task is not started";
+        }
+    }
+}
--- javaspring/src/main/java/patterns/sample/services/TimeOutTask.java
+++ javaspring/src/main/java/patterns/sample/services/TimeOutTask.java
@@ -4,11 +4,25 @@ import java.util.concurrent.Callable;
 
 public class TimeOutTask implements Callable<String> {
 
+    private int time;
+
+    public TimeOutTask() {
+        this.time = 5000;
+    }
+
+    /**
+     * Init a time out task with time
+     * @param time millisecond
+     */
+    public TimeOutTask(int time) {
+        this.time = time;
+    }
+
     @Override
     public String call() throws Exception {
-        //replace the below code with your long running operation
-        Thread.sleep(5000);
+        // replace the below code with your long running operation
+        Thread.sleep(time);
         return "Return after sleep 5 seconds";
     }
-    
+
 }
