b8ffaa640e8a2e3392907a434a02e5ae909f2e19
Tuan Tran <4468525+huntertran@users.noreply.github.com>
2021-04-29 11:23:22 -0400

    update PUT for dotnet

--- dotnet/Controllers/PostPutPatchReturnController.cs
+++ dotnet/Controllers/PostPutPatchReturnController.cs
@@ -7,7 +7,7 @@ namespace sampleApi.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
-    public class PostPutPatchReturnController : ControllerBase
+    public class PostPutPatchReturnController : CommonControllerLogics
     {
         private IStudentRepository _studentRepository;
 
@@ -22,25 +22,30 @@ namespace sampleApi.Controllers
             return _studentRepository.GetStudents();
         }
 
+        /// <summary>
+        /// Create a new student
+        /// </summary>
+        /// <param name="student">student info, in json format in request body</param>
+        /// <param name="isReturnObject">true if the client want to confirmed the student was inserted to the database</param>
+        /// <returns>the student or 200 ok status code</returns>
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Student student, bool isReturnObject)
         {
             _studentRepository.InsertStudent(student);
             var isSuccess = await _studentRepository.SaveAsync();
 
-            if (isSuccess)
-            {
-                if (isReturnObject)
-                {
-                    return Ok(student);
-                }
-                else
-                {
-                    return Ok();
-                }
-            }
+            return HandleReturnObjectLogic(isSuccess, isReturnObject, student, "Cannot insert new student");
+        }
 
-            return StatusCode(500, "Cannot insert new student");
+        [HttpPut]
+        public async Task<IActionResult> ChangeStudentEmail([FromBody] Student student, bool isReturnObject)
+        {
+            var dbStudent = await _studentRepository.GetStudentByID(student.Id);
+            dbStudent.Email = student.Email;
+
+            var isSuccess = await _studentRepository.SaveAsync();
+
+            return HandleReturnObjectLogic(isSuccess, isReturnObject, dbStudent, "Cannot change student email");
         }
     }
 }
---
+++ dotnet/Utils/PostPutPatchReturn/CommonControllerLogics.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sampleApi.Utils.PostPutPatchReturn
+{
+    public abstract class CommonControllerLogics : ControllerBase
+    {
+        public IActionResult HandleReturnObjectLogic(bool isSuccess,
+                                                     bool isReturnObject,
+                                                     object result,
+                                                     string message)
+        {
+            if (isSuccess)
+            {
+                if (isReturnObject)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return Ok();
+                }
+            }
+
+            return StatusCode(500, message);
+        }
+    }
+}
